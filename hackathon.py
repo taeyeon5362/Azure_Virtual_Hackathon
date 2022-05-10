8442168c150a192c7a9cf06da2833b910f01fbb6
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-11 03:05:50 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+import altair as alt    
+import plotly.express as px
+import os
 
-st.title("Title")
+#제목
+st.title('A 프로그램')
+st.subheader('프로그램 설명 | 날짜 : 2022.01.01 ~ 2022.01.02 | 용량 : 2.03 GB | 카테고리 : A ')
