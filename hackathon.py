d3fb9d4390d13ccac0e190ec6cccb2603698bb55
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-11 03:15:41 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,12 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
-import seaborn as sns
-import altair as alt    
-import plotly.express as px
+
 
 
 #제목
