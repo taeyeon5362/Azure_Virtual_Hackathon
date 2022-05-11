1c087fd3ebae9aaf97ccec7d45d16c40374b73c8
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-12 00:18:54 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -6,7 +6,7 @@ import numpy as np
 
 #제목
 st.title('A 프로그램')
-st.subheader('프로그램 설명  |  날짜 : 2022.01.01 ~ 2022.01.02  |  용량 : 2.03 GB  |  카테고리 : A ')
+st.header('프로그램 설명  |  날짜 : 2021.07.10 ~ 2021.07.11  |  용량 : 2.03 GB  |  카테고리 : A ')
 st.subheader(' ')
 st.subheader(' ')
 
@@ -19,16 +19,17 @@ time.set_index = time['timestamp']
 data_1 = pd.read_csv('./csv/validation_data_1.csv', encoding='cp949')
 data_1.set_index = data_1['timestamp']
 
-
+data_2 = pd.read_csv('./csv/validation_data_2.csv', encoding='cp949')
+data_2.set_index = data_2['timestamp']
 
 
 #ai 그래프
 
-st.subheader('보안 데이터 수치')
+st.header('보안 데이터 수치')
 st.subheader(' ')
 
 
-st.write('시간별 데이터 비교')
+st.subheader('시간별 데이터 비교')
 
 time = time.groupby('timestamp').sum()
 times = time.index.tolist()
@@ -39,7 +40,11 @@ time_index = time_data.index.tolist()
 st.line_chart(time_data.loc[time_index[0]], use_container_width=True)
 
 
-st.write('데이터별 시간 데이터 비교')
+st.subheader('데이터별 시간 데이터 비교')
+st.subheader(' ')
+
+
+st.write('시간별 비교')
 
 data_1 = data_1.groupby('timestamp').sum()
 datas_1 = data_1.index.tolist()
@@ -49,4 +54,13 @@ data_data_1 = data_1.loc[(data_1.index == option)]
 data_index_1 = data_data_1.index.tolist()
 st.line_chart(data_data_1.loc[data_index_1[0]], use_container_width=True)
 
+st.subheader(' ')
+st.write('한시간 비교')
 
+data_2 = data_2.groupby('timestamp').sum()
+datas_2 = data_2.index.tolist()
+option = st.selectbox('Select Data 2',(datas_2))
+
+data_data_2 = data_2.loc[(data_2.index == option)]
+data_index_2 = data_data_2.index.tolist()
+st.line_chart(data_data_2.loc[data_index_2[0]], use_container_width=True)
