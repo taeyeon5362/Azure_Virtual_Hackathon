05383711aedf710809d45cbe7b2a01b9939fe913
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-11 23:34:39 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -16,7 +16,8 @@ st.subheader(' ')
 time = pd.read_csv('./csv/validation_time.csv', encoding='cp949')
 time.set_index = time['timestamp']
 
-
+data_1 = pd.read_csv('./csv/validation_data_1.csv', encoding='cp949')
+data_1.set_index = data_1['timestamp']
 
 
 
@@ -38,6 +39,16 @@ time_data = time.loc[(time.index == option)]
 time_index = time_data.index.tolist()
 st.line_chart(time_data.loc[time_index[0]], use_container_width=True)
 
+
+st.write('데이터별 시간 데이터 비교')
+
+data_1 = data_1.groupby('timestamp').sum()
+datas_1 = data_1.index.tolist()
+option = st.selectbox('Select Data 1',(datas_1))
+
+data_data_1 = data_1.loc[(data_1.index == option)]
+data_index_1 = data_data_1.index.tolist()
+st.line_chart(data_data_1.loc[data_index_1[0]], use_container_width=True)
 
 
 
