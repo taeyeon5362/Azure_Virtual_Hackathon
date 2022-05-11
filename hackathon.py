08131538babd1ff301dbe87492c0c2318bc8621a
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-11 23:31:49 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -43,6 +43,8 @@ time_index = time_data.index.tolist()
 st.line_chart(time_data.loc[time_index[0]], use_container_width=True)
 
 
+st.write('데이터별 시간 데이터 비교')
+
 data_1 = data_1.groupby('timestamp').sum()
 datas_1 = data_1.index.tolist()
 option = st.selectbox('Select Data 1',(datas_1))
