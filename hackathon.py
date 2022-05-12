236e16d3105199a7c387591eda8e9799c39ebb72
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-12 21:20:46 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -7,7 +7,7 @@ import altair as alt
 
 #제목
 st.title('SAI Board - cyber Security Advisor')
-st.header('cyber Security Advisor | Example 프로그램 | 날짜 : 2021.07.10 ~ 2021.07.11  |  용량 : 2.03 GB  |  카테고리 : A ')
+st.header('Cyber Security Advisor | Example 프로그램 | 날짜 : 2021.07.10 ~ 2021.07.11  |  용량 : 2.03 GB  |  카테고리 : A ')
 st.subheader(' ')
 st.subheader(' ')
 
