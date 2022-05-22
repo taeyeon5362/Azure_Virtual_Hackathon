c9c9e751da2853c0839fe238319937d0bf399620
taeyeon5362 <51779879+taeyeon5362@users.noreply.github.com>
2022-05-22 20:49:48 +0900

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -14,8 +14,8 @@ st.subheader(' ')
 #데이터 불러오기
 
 
-time = pd.read_csv('./csv/validation_time.csv', encoding='cp949')
-time.set_index = time['timestamp']
+#time = pd.read_csv('./csv/validation_time.csv', encoding='cp949')
+#time.set_index = time['timestamp']
 
 data_1 = pd.read_csv('./csv/validation_data_1.csv', encoding='cp949')
 data_1.set_index = data_1['timestamp']
@@ -44,13 +44,13 @@ st.subheader(' ')
 
 st.subheader('시간별 데이터 비교')
 
-time = time.groupby('timestamp').sum()
-times = time.index.tolist()
-option = st.selectbox('Select Time',(times))
+#time = time.groupby('timestamp').sum()
+#times = time.index.tolist()
+#option = st.selectbox('Select Time',(times))
 
-time_data = time.loc[(time.index == option)]
-time_index = time_data.index.tolist()
-st.line_chart(time_data.loc[time_index[0]], use_container_width=True)
+#time_data = time.loc[(time.index == option)]
+#time_index = time_data.index.tolist()
+#st.line_chart(time_data.loc[time_index[0]], use_container_width=True)
 
 
 st.subheader('데이터별 시간 데이터 비교')
